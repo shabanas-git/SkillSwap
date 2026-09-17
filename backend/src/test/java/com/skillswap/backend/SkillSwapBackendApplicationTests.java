fb1a385b0b0a572c8f34f27ac553afac181b80d7
shabanas-git <shabanasam2007@gmail.com>
2026-09-17 18:38:52 +0530

    Setup SkillSwap Spring Boot backend

---
+++ backend/src/test/java/com/skillswap/backend/SkillSwapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillswap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
